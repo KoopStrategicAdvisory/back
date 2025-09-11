a8d29c7de47decdc4490d99e9549694870b95ec2
Felipe Rey <koopstrategicadvisory@gmail.com>
2025-09-11 16:23:26 -0500

    test

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:5173';
 );*/
 app.use(
   cors({
-    origin: true,
+    origin: "https://koopstrategicadvisory.com",
     methods: ["POST"],
     allowedHeaders: ["Content-Type"],
     preflightContinue: true
